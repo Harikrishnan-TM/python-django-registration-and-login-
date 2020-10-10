08a63afe995c8a26d69e957d6837727c28671f65
Harikrishnanonline <techdatamax@gmail.com>
2020-10-10 21:53:16 +0530

    now if the registration is successful it will be redirected to the login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('register/', views.register, name="register"),
-    path('login/', views.login, name="login"),
+    path('accounts/login/', views.login, name="login"),
     
     
     
